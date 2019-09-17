cf8131eca1913203e764336244fcf2b999aa2b65
Terrence Kuiper <github@terrencekuiper.com>
2019-09-17 00:10:46 +0000

    Add head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    console.log(`❗️❗️❗️ Assertion Failed: ${actual} !== ${expected} ❗️❗️❗️`);
+  }
+
+};
+
+let head = function(element) {
+  return element[0];
+};
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
