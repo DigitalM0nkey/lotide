676e5efdb23482171b6ecf2c33bd6240f7ddf8a9
Terrence Kuiper <github@terrencekuiper.com>
2019-09-23 12:45:11 -0700

    Refactored tail.js with export/require

--- tail.js
+++ tail.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`❗️❗️❗️ Assertion Failed: ${actual} !== ${expected} ❗️❗️❗️`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 let tail = function(element) {
   return element.slice(1);
 };
 
-console.log();
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
