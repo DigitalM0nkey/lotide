7013d4c143e2bdc52ad775f4970bdf15537e5851
Terrence Kuiper <github@terrencekuiper.com>
2019-09-23 12:59:13 -0700

    refactored eqArrays with export/require

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`❗️❗️❗️ Assertion Failed: ${actual} !== ${expected} ❗️❗️❗️`);
-  }
-
-};
-
 const eqArrays = (actArr, expArr) => {
   if (JSON.stringify(actArr) === JSON.stringify(expArr)) {
     return true;
@@ -15,16 +6,4 @@ const eqArrays = (actArr, expArr) => {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
