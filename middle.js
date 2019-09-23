310d1b044914dbed60af000b5b95b276678724cb
Terrence Kuiper <github@terrencekuiper.com>
2019-09-23 13:23:05 -0700

    Refactor middle to include export/require. Pair programming with @caitlining

--- middle.js
+++ middle.js
@@ -1,21 +1,4 @@
-const eqArrays = (actArr, expArr) => {
-  if (JSON.stringify(actArr) === JSON.stringify(expArr)) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-
-const assertEqualArrays = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`❗️❗️❗️ Assertion Failed: ${actual} !== ${expected} ❗️❗️❗️`);
-  }
-};
-
-const middle = function (array) {
+const middle = function(array) {
   let output = [];
   if (array.length <= 2) {
     output = [];
@@ -28,11 +11,4 @@ const middle = function (array) {
   return output;
 };
 
-
-
-
-assertEqualArrays(middle([1, 2, 3, 5, 4]), [3]);
-assertEqualArrays(middle([1, 2,]), []);
-assertEqualArrays(middle([1, 3, 4]), [3]);
-assertEqualArrays(middle([1, 2, 3, 4]), [2, 3]);
-assertEqualArrays(middle([1, 2, 3, 5, 4, 5]), [3, 5]);
+module.exports = middle;
